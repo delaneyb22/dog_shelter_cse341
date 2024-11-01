c98e32d67349fa4a06dbedd4be2ddcaa2a8e3bfb
delaneyb22 <delaneybaker8@gmail.com>
2024-11-01 16:04:33 -0500

    dogs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./swagger'));
-router.use('/dogs', require('./dogs'));
+router.use('/', require('./dogs'));
 //router.use('/shelters', require('./shelters'));
 //router.use('/users', require('./users'));
 //router.use('/auth', require('./auth'));
