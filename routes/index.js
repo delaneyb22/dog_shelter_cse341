f3aa3122eec584676a9da538690615232ec0dc20
delaneyb22 <delaneybaker8@gmail.com>
2024-11-01 16:29:07 -0500

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const dog = require('./dogs');
 
 router.use('/', require('./swagger'));
-router.get('/dogs', dog.getDogs);
-router.get('/dogs/:id', dog.getDog);
+router.use('/dogs', require('./dogs'));
 //router.use('/shelters', require('./shelters'));
 //router.use('/users', require('./users'));
 //router.use('/auth', require('./auth'));
