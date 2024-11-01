f6575aa1d97c89c759798ac97a21711904b0c1ab
delaneyb22 <delaneybaker8@gmail.com>
2024-11-01 15:59:53 -0500

    https

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'Dog Shelter API',
     description: 'API for managing dogs in a shelter',
   },
-  host: 'https://dog-shelter-cse341.onrender.com', // Use an environment variable for the host
-  schemes: ['http', 'https'],
+  host: 'dog-shelter-cse341.onrender.com', // Use an environment variable for the host
+  schemes: ['https', 'http'],
   version: '1.0.0', // Optional: add versioning
 };
 
